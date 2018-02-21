d898d399086c7a7694f72425b0e4f0776c0d68bc
Edgar <edgar@restartinformatica.com.br>
2018-02-21 17:23:34 -0300

    Melhorias no Handler de erros

--- src/app/shared/Helpers/ApplicationErrorHandler.ts
+++ src/app/shared/Helpers/ApplicationErrorHandler.ts
@@ -8,55 +8,59 @@ import {isNullOrUndefined} from 'util';
 @Injectable()
 export class ApplicationErrorHandler extends  ErrorHandler {
 
-    constructor (private notificationService: NotificationService, private zone: NgZone) {
-        super();
-    }
+  constructor (private notificationService: NotificationService, private zone: NgZone) {
+    super();
+  }
 
-    GetPropertyValue(object: any, propertyName: string) {
-        for (const key in object) {
-            if (key === propertyName) {
-                return object[key];
-            }
+  GetPropertyValue(object: any, propertyName: string) {
+    for (const key in object) {
+      if (key === propertyName) {
+        return object[key];
+      }
+    }
+  }
+
+  handleError(errorResponse: HttpErrorResponse | any) {
+    console.log('HandleError');
+    console.log('ErrorResponse -> ' + JSON.stringify(errorResponse));
+    if (errorResponse instanceof HttpErrorResponse) {
+      console.log('HandleError passo 01');
+      const error = errorResponse.error;
+      let msg = null;
+      if (!isNullOrUndefined(error)) {
+        if (!isNullOrUndefined(error.mensagem)) {
+          msg = error.mensagem;
         }
-    }
-
-    handleError(errorResponse: HttpErrorResponse | any) {
-        console.log('HandleError');
-        console.log('ErrorResponse -> ' + JSON.stringify(errorResponse));
-        if (errorResponse instanceof HttpErrorResponse) {
-            console.log('HandleError passo 01');
-            const error = errorResponse.error;
-            let msg = null;
-            if (!isNullOrUndefined(error)) {
-                if (!isNullOrUndefined(error.mensagem)) {
-                    msg = error.mensagem;
-                }
-            }
-            if (isNullOrUndefined(msg)) {
-                switch (errorResponse.status) {
-                    case 401:
-                        msg = (errorResponse.message || 'Você não está autenticado');
-                        break;
-                    case 403:
-                        msg = 'Não autorizado';
-                        break;
-                    case  404:
-                        msg = 'Página não localizada';
-                        break;
-                    default:
-                        msg = errorResponse.message;
-                        break;
-                }
-            }
-            console.log('HandleError passo 04 ' + msg);
-            this.zone.run(() => {
-                this.notificationService.AlertBoxError(msg);
-            });
-
+      }
+      console.log('HandleError passo 04 '+error);
+      if (isNullOrUndefined(msg)) {
+        switch (errorResponse.status) {
+          case 401:
+            msg = (errorResponse.message || 'Você não está autenticado');
+            break;
+          case 403:
+            msg = 'Não autorizado';
+            break;
+          case  404:
+            msg = 'Página não localizada';
+            break;
+          case 500:
+            msg = (error || 'Aconteceu um erro inesperado no servidor, segue os detalhes '+errorResponse.message);
+            break;
+          default:
+            msg = errorResponse.message;
+            break;
         }
-        console.log('HandleError Fim');
-        super.handleError(errorResponse);
+      }
+      console.log('HandleError passo 05 ' + msg);
+      this.zone.run(() => {
+        this.notificationService.AlertBoxError(msg);
+      });
+
     }
+    console.log('HandleError Fim');
+    super.handleError(errorResponse);
+  }
 
 
 }
